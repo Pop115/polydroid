4cf369786b3dac9c7b4c36ce143e9102bdad2d7b
Kienan Bachwa <kienan.bachwa@etu.unice.fr>
2018-05-07 17:19:37 +0200

    ajout serveur

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const {Client} = require('pg');
+
+const app = express();
+app.disable('x-powered-by');
+app.use(cors());
+app.use(bodyParser.json({}));
+app.use(morgan('dev'));
+
+
+const client = new Client({
+	connectionString: 'postgres://ztiuuvpd:rs0KSwq8kJyOF3ze8XOM3P8yvBbucJgv@horton.elephantsql.com:5432/ztiuuvpd'
+});
+
+client.connect()
+	.then(() => {
+		console.log('[' + new Date().toISOString() + '] Connect to Postgres OK ');
+		const server = app.listen(process.env.PORT || 3000, () => {
+			console.log('[' + new Date().toISOString() + '] Server launched on port ' + server.address().port + '!');
+		});
+	})
+	.catch(err => {
+		console.log(err);
+	});
+
+
+app.get('/incidents', async (req, res) => {
+	try {
+		const result = await client.query('SELECT * FROM "Incidents"');
+		console.log("incidents:  " + result.rows[0].message);
+		res.status(200).json(result.rows);
+	}
+	catch (err) {
+		res.status(500).json(err);
+	}
+});
