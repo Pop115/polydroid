2e937957269658e6c4bf9005c85cac5cbd81a5f4
Kienan Bachwa <kienan.bachwa@etu.unice.fr>
2018-05-13 01:11:54 +0200

    Ajout gradle + affichage même si serveur indisponible

--- app/src/main/java/si3/polytech/polydroid/AsyncDBHelpertruc.java
+++ app/src/main/java/si3/polytech/polydroid/AsyncDBHelpertruc.java
@@ -1,10 +1,5 @@
 package si3.polytech.polydroid;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 
 import org.json.JSONArray;
@@ -12,47 +7,48 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 
-public class IncidentDBHelper {
+public class AsyncDBHelpertruc extends AsyncTask<Void, Void, ArrayList<Incident>> {
 
-
-
-    public List<Incident> getAllArticles() {
-        ArrayList<Incident> incidentArrayList = new ArrayList<>();
-        JSONArray incidentJSON = null;
-        String stringFromURL = "";
+    @Override
+    protected ArrayList<Incident> doInBackground(Void... voids) {
         try {
-            AsyncTask<Object, Object, String> asyncTask = new AsyncTask() {
-                @Override
-                protected Object doInBackground(Object[] objects) {
-                    return getStringFromURL("http://192.168.43.66:3000/incidents");
-                }
-            };
-            stringFromURL = asyncTask.execute().get();
-            incidentJSON = new JSONArray(stringFromURL);
+            return getAllArticles();
         } catch (JSONException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+            return null;
         }
+    }
 
-        for(int i = 0; i<incidentJSON.length(); i++){
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        System.out.println("-----------Loading-----------");
+    }
+
+
+    @Override
+    protected void onPostExecute(ArrayList<Incident> result) {
+        super.onPostExecute(result);
+        //incidentArrayList.addAll(result);
+    }
+
+    public ArrayList<Incident> getAllArticles() throws JSONException {
+        ArrayList<Incident> incidentArrayList = new ArrayList<>();
+        JSONArray incidentJSON = null;
+        String stringFromURL = getStringFromURL("http://192.168.1.34:3000/incidents");
+
+        incidentJSON = new JSONArray(stringFromURL);
+
+
+        for (int i = 0; i < incidentJSON.length(); i++) {
             try {
                 JSONObject jsonObject = incidentJSON.getJSONObject(i);
                 Date date = new Date(jsonObject.getString("date"));
@@ -71,13 +67,6 @@ public class IncidentDBHelper {
 
         }
 
-        /*
-        Cursor cursor = myDataBase.rawQuery("SELECT * FROM Incidents", null);
-        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-            System.out.println(cursor);
-        }
-        cursor.close();
-        */
 
         Incident newIncident = new Incident(new Date(), "Moi", new Localisation("TEMPLIERS", "E+155", "jolie salle"), "Chaise cassée, gros problème!", "Chaise cassée", Importance.Critique, Type.ELEC);
         incidentArrayList.add(newIncident);
@@ -110,10 +99,12 @@ public class IncidentDBHelper {
             in.close();
         } catch (Exception e) {
             e.printStackTrace();
-            System.err.println("Exception while calling URL:"+ myURL);
+            System.err.println("Exception while calling URL:" + myURL);
         }
 
         return sb.toString();
     }
 
 }
+
+
--- app/src/main/java/si3/polytech/polydroid/IncidentFragment.java
+++ app/src/main/java/si3/polytech/polydroid/IncidentFragment.java
@@ -1,6 +1,9 @@
 package si3.polytech.polydroid;
 
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,9 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.io.IOException;
-import java.sql.SQLException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Kienan on 25/03/2018.
@@ -21,8 +32,7 @@ public class IncidentFragment extends Fragment {
      * The fragment argument representing the section number for this
      * fragment.
      */
-    ArrayList<Incident> incidentArrayList = new ArrayList<>();
-    IncidentDBHelper newsDBHelper;
+    CustomRecyclerAdapter adapter = new CustomRecyclerAdapter();
 
     public IncidentFragment() {
 
@@ -50,20 +60,115 @@ public class IncidentFragment extends Fragment {
     public void onActivityCreated(Bundle bundle) {
         super.onActivityCreated(bundle);
 
-        newsDBHelper = new IncidentDBHelper();
-        incidentArrayList.addAll(newsDBHelper.getAllArticles());
-
-        //NewsCustomAdapter arrayAdapter = new NewsCustomAdapter(this.getContext(), android.R.layout.simple_list_item_1, articleList);
-        //GridView gridView = (GridView) this.getView().findViewById(R.id.grid);
-        //gridView.setAdapter(arrayAdapter);
-
         RecyclerView recyclerView = (RecyclerView) this.getView().findViewById(R.id.recycler);
-        CustomRecyclerAdapter adapter = new CustomRecyclerAdapter();
-        adapter.incidentArrayList.addAll(incidentArrayList);
         recyclerView.setAdapter(adapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getContext());
         recyclerView.setLayoutManager(linearLayoutManager);
+        AsyncDBHelper incidentDBHelper = new AsyncDBHelper();
+        incidentDBHelper.execute();
 
     }
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
+    private class AsyncDBHelper extends AsyncTask<Void, Void, ArrayList<Incident>> {
+
+        @Override
+        protected ArrayList<Incident> doInBackground(Void... voids) {
+            try {
+                return getAllArticles();
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            System.out.println("-----------Loading-----------");
+        }
+
+
+        @Override
+        protected void onPostExecute(ArrayList<Incident> result) {
+            super.onPostExecute(result);
+            System.out.println(result);
+            if(result != null)
+                adapter.incidentArrayList .addAll(result);
+            adapter.notifyDataSetChanged();
+        }
+
+        public ArrayList<Incident> getAllArticles() throws JSONException {
+            ArrayList<Incident> incidentArrayList = new ArrayList<>();
+            JSONArray incidentJSON = null;
+            String stringFromURL = getStringFromURL("http://192.168.1.34:3000/incidents");
+
+            incidentJSON = new JSONArray(stringFromURL);
+
+
+            for (int i = 0; i < incidentJSON.length(); i++) {
+                try {
+                    JSONObject jsonObject = incidentJSON.getJSONObject(i);
+                    Date date = new Date(jsonObject.getString("date"));
+                    String auteur = jsonObject.getString("auteur");
+                    String localisation = jsonObject.getString("localisation");
+                    String description = jsonObject.getString("description");
+                    String titre = jsonObject.getString("titre");
+                    String type = jsonObject.getString("type");
+                    String importance = jsonObject.getString("importance");
+                    String URLImage = jsonObject.getString("URLMiniature");
+                    int id = jsonObject.getInt("id");
+                    incidentArrayList.add(new Incident(date, auteur, new Localisation("", "", localisation), description, titre, Importance.valueOf(importance), Type.valueOf(type)));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+            Incident newIncident = new Incident(new Date(), "Moi", new Localisation("TEMPLIERS", "E+155", "jolie salle"), "Chaise cassée, gros problème!", "Chaise cassée", Importance.Critique, Type.ELEC);
+            incidentArrayList.add(newIncident);
+
+            return incidentArrayList;
+        }
+
+
+        public String getStringFromURL(String myURL) {
+            StringBuilder sb = new StringBuilder();
+            URLConnection urlConn = null;
+            InputStreamReader in = null;
+            try {
+                URL url = new URL(myURL);
+                urlConn = url.openConnection();
+                if (urlConn != null)
+                    urlConn.setReadTimeout(60000);
+                if (urlConn != null && urlConn.getInputStream() != null) {
+                    in = new InputStreamReader(urlConn.getInputStream(),
+                            Charset.defaultCharset());
+                    BufferedReader bufferedReader = new BufferedReader(in);
+                    if (bufferedReader != null) {
+                        int cp;
+                        while ((cp = bufferedReader.read()) != -1) {
+                            sb.append((char) cp);
+                        }
+                        bufferedReader.close();
+                    }
+                }
+                in.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.err.println("Exception while calling URL:" + myURL);
+            }
+
+            return sb.toString();
+        }
+
+    }
+
+
+
 
 }
